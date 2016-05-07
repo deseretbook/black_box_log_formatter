87e0c963754b8cf4bc9dff514e51692beb32931d
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2016-05-06 18:06:19 -0600

    Move AwesomeBacktrace from the BlackBox Framework project.

--- lib/black_box_log_formatter.rb
+++ lib/black_box_log_formatter.rb
@@ -3,4 +3,5 @@ require 'black_box_log_formatter/version'
 module BlackBoxLogFormatter
 end
 
+require 'black_box/awesome_backtrace'
 require 'black_box/log_formatter'
--- lib/black_box_log_formatter/version.rb
+++ lib/black_box_log_formatter/version.rb
@@ -1,3 +1,3 @@
 module BlackBoxLogFormatter
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
